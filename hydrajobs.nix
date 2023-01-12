d1ae3f16194d94a6a2038afcdb70ff8968da165e
Mika Tammi <mika.tammi@unikie.com>
2023-01-12 22:57:51 +0200

    Hydra Job for NVIDIA Jetson Orin cross-compilation
    Hydra Job for package:
    * aarch64-linux-cross-from-x86_64-linux.nvidia-jetson-orin
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- hydrajobs.nix
+++ hydrajobs.nix
@@ -1,6 +1,10 @@
 {self}: {
   hydraJobs = {
     intel-nuc.x86_64-linux = self.packages.x86_64-linux.intel-nuc;
-    nvidia-jetson-orin.aarch64-linux = self.packages.aarch64-linux.nvidia-jetson-orin;
+    vm.x86_64-linux = self.packages.x86_64-linux.vm;
+    nvidia-jetson-orin = {
+      aarch64-linux = self.packages.aarch64-linux.nvidia-jetson-orin;
+      x86_64-linux = self.packages.aarch64-linux-cross-from-x86_64-linux.nvidia-jetson-orin;
+    };
   };
 }
