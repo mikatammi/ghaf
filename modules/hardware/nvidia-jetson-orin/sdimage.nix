f3955e8f6ed79f89f5284c12b97090e518ff42e6
Alexander V. Nikolaev <avn@avnik.info>
2023-08-04 14:11:44 +0300

    cleanup: remove snippets generated from bootspec

--- modules/hardware/nvidia-jetson-orin/sdimage.nix
+++ modules/hardware/nvidia-jetson-orin/sdimage.nix
@@ -18,28 +18,7 @@
       isExecutable = true;
       inherit (pkgs.buildPackages) python3;
     };
-    kernelPath = "${config.boot.kernelPackages.kernel}/" + "${config.system.boot.loader.kernelFile}";
-    initrdPath = "${config.system.build.initialRamdisk}/" + "${config.system.boot.loader.initrdFile}";
     fdtPath = "${config.hardware.deviceTree.package}/${config.hardware.deviceTree.name}";
-    loaderConf = pkgs.writeText "loader.conf" ''
-      timeout 0
-      default nixos-generation-1.conf
-      console-mode keep
-    '';
-    entriesSrel = pkgs.writeText "entries.srel" ''
-      type1
-
-    '';
-    entry = pkgs.writeText "nixos-generation-1.conf" ''
-      title NixOS
-      version Generation 1
-      linux /EFI/nixos/${config.system.boot.loader.kernelFile}
-      initrd /EFI/nixos/${config.system.boot.loader.initrdFile}
-      options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
-      machine-id 0123456789abcdef0123456789abcdef
-
-      devicetree /EFI/nixos/${config.hardware.deviceTree.name}
-    '';
   in {
     firmwareSize = 256;
     # TODO: Replace contents of the populateFirmwareCommands with proper
