cd71e0b38b2d66bdc8684bddfbbbe669df302e89
Mika Tammi <mika.tammi@unikie.com>
2023-09-20 18:48:14 +0300

    Tanel's buildPkcs11Ta patch
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- targets/nvidia-jetson-orin.nix
+++ targets/nvidia-jetson-orin.nix
@@ -73,6 +73,64 @@
             };
           }
 
+          (
+            {
+              pkgs,
+              config,
+              ...
+            }: let
+              stdenv = pkgs.gcc9Stdenv;
+              inherit (jetpack-nixos.legacyPackages.${config.nixpkgs.buildPlatform.system}) bspSrc l4tVersion;
+              inherit
+                (pkgs.callPackages (jetpack-nixos + "/optee.nix") {
+                  inherit bspSrc l4tVersion stdenv;
+                })
+                opteeClient
+                buildOpteeTaDevKit
+                ;
+              pcks11Ta = stdenv.mkDerivation {
+                pname = "pkcs11";
+                version = l4tVersion;
+                src = pkgs.fetchgit {
+                  url = "https://nv-tegra.nvidia.com/r/tegra/optee-src/nv-optee";
+                  rev = "jetson_${l4tVersion}";
+                  sha256 = "sha256-44RBXFNUlqZoq3OY/OFwhiU4Qxi4xQNmetFmlrr6jzY=";
+                };
+                nativeBuildInputs = [(pkgs.buildPackages.python3.withPackages (p: [p.cryptography]))];
+                makeFlags = [
+                  "-C optee/optee_os/ta/pkcs11"
+                  "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
+                  "TA_DEV_KIT_DIR=${buildOpteeTaDevKit {
+                    socType = "t234";
+                    inherit (config.hardware.nvidia-jetpack.firmware.optee) taPublicKeyFile;
+                    opteePatches = config.hardware.nvidia-jetpack.firmware.optee.patches;
+                    extraMakeFlags = config.hardware.nvidia-jetpack.firmware.optee.extraMakeFlags;
+                  }}/export-ta_arm64"
+                  "CFG_PKCS11_TA_TOKEN_COUNT=3"
+                  "CFG_PKCS11_TA_HEAP_SIZE=32768"
+                  "CFG_PKCS11_TA_AUTH_TEE_IDENTITY=y"
+                  "CFG_PKCS11_TA_ALLOW_DIGEST_KEY=y"
+                  "OPTEE_CLIENT_EXPORT=${opteeClient}"
+                  "O=$(PWD)/out"
+                ];
+                installPhase = ''
+                  runHook preInstall
+                  install -Dm755 -t $out out/*.stripped.elf
+                  runHook postInstall
+                '';
+              };
+            in {
+              hardware.nvidia-jetpack.firmware.optee.clientLoadPath = pkgs.linkFarm "optee-load-path" [
+                {
+                  # TODO: Figure out what filename the tee_supplicant expects to find
+                  name = "optee_armtz/fd02c9da-306c-48c7-a49c-bbd827ae86ee.ta";
+                  # TODO: Maybe exact path to the TA file
+                  path = "${pcks11Ta}/fd02c9da-306c-48c7-a49c-bbd827ae86ee.stripped.elf";
+                }
+              ];
+            }
+          )
+
           formatModule
         ]
         ++ (import ../modules/module-list.nix)
