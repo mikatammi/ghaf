15cd011a22939782a2dd601cc324acfe0bcb6d00
Brian McGillion <bmg.avoin@gmail.com>
2023-11-23 10:31:24 +0400

    Fix up packages references
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- overlays/custom-packages/gala/default.nix
+++ overlays/custom-packages/gala/default.nix
@@ -1,5 +1,5 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 (final: _prev: {
-  gala-app = final.callPackage ../../../user-apps/gala {};
+  gala-app = final.callPackage ../../../packages/gala {};
 })
