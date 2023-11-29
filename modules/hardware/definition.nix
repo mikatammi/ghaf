df11c39833534e6c18b445caf46a573a3663ca63
Mika Tammi <mika.tammi@unikie.com>
2023-11-29 15:45:01 +0200

    squash! WIP: Initial hardware definition module
    Add header comment and missing lib parameter
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- modules/hardware/definition.nix
+++ modules/hardware/definition.nix
@@ -1,4 +1,11 @@
-_: {
+# Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Module for Hardware Definitions
+#
+# The point of this module is to only store information about the hardware
+# configuration, and the logic that uses this information should be elsewhere.
+{lib, ...}: {
   options.ghaf.hardware.definition = with lib; let
     pciDevSubmodule = types.submodule {
       path = mkOption {
