0d739b84ff435fcf6dff8b63be15289d43fc3483
Mika Tammi <mika.tammi@unikie.com>
2024-03-06 00:07:59 +0200

    Move graphics profile under desktop modules
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- modules/common/profiles/default.nix
+++ modules/common/profiles/default.nix
@@ -4,7 +4,6 @@
   imports = [
     ./applications.nix
     ./debug.nix
-    ./graphics.nix
     ./release.nix
   ];
 }
--- modules/desktop/profiles/default.nix
+++ modules/desktop/profiles/default.nix
@@ -1,11 +1,7 @@
 # Copyright 2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-#
-# Configurations for various Wayland compositors
-#
 {
   imports = [
-    ./graphics
-    ./profiles
+    ./graphics.nix
   ];
 }
