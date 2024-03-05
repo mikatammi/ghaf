4dfe16db59169101d53aa356c3f56a0289418ae5
Mika Tammi <mika.tammi@unikie.com>
2024-03-05 23:54:23 +0200

    Refactorize microvm
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

---
+++ modules/microvm/default.nix
@@ -0,0 +1,15 @@
+# Copyright 2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Implementation of ghaf's virtual machines based on microvm.nix
+#
+{
+  imports = [
+    ./virtualization/microvm/common/vm-networking.nix
+    ./virtualization/microvm/microvm-host.nix
+    ./virtualization/microvm/netvm.nix
+    ./virtualization/microvm/appvm.nix
+    ./virtualization/microvm/guivm.nix
+    ./networking.nix
+  ];
+}
