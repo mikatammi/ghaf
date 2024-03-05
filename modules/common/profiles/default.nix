4b4d2abef6a47cc0c846442b864bba709f5f461e
Mika Tammi <mika.tammi@unikie.com>
2024-03-06 00:54:46 +0200

    Move applications profile under desktop
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- modules/common/profiles/default.nix
+++ modules/common/profiles/default.nix
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   imports = [
-    ./applications.nix
     ./debug.nix
     ./release.nix
   ];
--- modules/desktop/profiles/default.nix
+++ modules/desktop/profiles/default.nix
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   imports = [
+    ./applications.nix
     ./graphics.nix
   ];
 }
