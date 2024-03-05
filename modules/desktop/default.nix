350adb275ab69dea94c5f25ceed4fb7d46935653
Mika Tammi <mika.tammi@unikie.com>
2024-03-05 23:55:10 +0200

    Refactorize desktop
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

---
+++ modules/desktop/default.nix
@@ -0,0 +1,10 @@
+# Copyright 2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Configurations for various Wayland compositors
+#
+{
+  imports = [
+    ./graphics
+  ];
+}
