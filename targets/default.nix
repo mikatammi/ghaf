48230546efcadf1859f0634f577fee41d312b45f
Mika Tammi <mika.tammi@unikie.com>
2024-03-06 02:48:41 +0200

    Fix IMX8
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- targets/default.nix
+++ targets/default.nix
@@ -15,6 +15,6 @@ in
     (import ./generic-x86_64.nix {inherit self lib nixos-generators microvm;})
     (import ./lenovo-x1 {inherit self lib microvm lanzaboote disko;})
     (import ./lenovo-x1-carbon-installer.nix {inherit self lib;})
-    (import ./imx8qm-mek.nix {inherit lib nixos-generators nixos-hardware microvm;})
+    (import ./imx8qm-mek.nix {inherit self lib nixos-generators nixos-hardware microvm;})
     (import ./microchip-icicle-kit.nix {inherit self lib nixpkgs nixos-hardware;})
   ]
