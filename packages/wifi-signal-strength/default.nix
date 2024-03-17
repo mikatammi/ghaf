e1370017d7490a14d4ff9167f11af8ec353997db
Mika Tammi <mika.tammi@unikie.com>
2024-03-17 21:01:30 +0200

    WIP: Refactor: Simplify wifi-signal-strength
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- packages/wifi-signal-strength/default.nix
+++ packages/wifi-signal-strength/default.nix
@@ -1,21 +1,23 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  stdenvNoCC,
-  pkgs,
+  gawk,
   lib,
+  networkmanager,
+  openssh,
+  writeShellApplication,
   wifiDevice,
-  ...
 }: let
   # Replace the IP address with "net-vm.ghaf" after DNS/DHCP module merge
   netvm_address = "192.168.100.1";
-  wifiSignalStrength =
-    pkgs.writeShellScript
-    "wifi-signal-strength"
-    ''
+in
+  pkgs.writeShellApplication {
+    name = "wifi-signal-strength";
+
+    text = ''
       export DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/ssh_session_dbus.sock
       export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/tmp/ssh_system_dbus.sock
-      ${pkgs.openssh}/bin/ssh -M -S /tmp/nmcli_socket \
+      ${openssh}/bin/ssh -M -S /tmp/nmcli_socket \
           -f -N -q ghaf@${netvm_address} \
           -i /run/waypipe-ssh/id_ed25519 \
           -o StrictHostKeyChecking=no \
@@ -29,9 +31,9 @@
       signal3=󰤨
       no_signal=󰤭
       # Get IP address of netvm
-      address=$(${pkgs.networkmanager}/bin/nmcli device show ${wifiDevice} | ${pkgs.gawk}/bin/awk '{ if ($1=="IP4.ADDRESS[1]:") {print $2}}')
+      address=$(${networkmanager}/bin/nmcli device show ${wifiDevice} | ${gawk}/bin/awk '{ if ($1=="IP4.ADDRESS[1]:") {print $2}}')
       # Get signal strength and ssid
-      connection=($(${pkgs.networkmanager}/bin/nmcli -f IN-USE,SIGNAL,SSID dev wifi | ${pkgs.gawk}/bin/awk '/^\*/{if (NR!=1) {print $2; print $3}}'))
+      connection=($(${networkmanager}/bin/nmcli -f IN-USE,SIGNAL,SSID dev wifi | ${gawk}/bin/awk '/^\*/{if (NR!=1) {print $2; print $3}}'))
       connection[0]=$(if [ -z ''${connection[0]} ]; then echo "-1"; else echo ''${connection[0]}; fi)
       # Set the icon of signal level
       signal_level=$(if [ ''${connection[0]} -gt 80 ]; then echo $signal3; elif [ ''${connection[0]} -gt 60 ]; then echo $signal2; elif [ ''${connection[0]} -gt 30 ]; then echo $signal1; elif [ ''${connection[0]} -gt 0 ]; then echo signal0; else echo $no_signal; fi)
@@ -40,17 +42,7 @@
       # Return as json format for waybar
       echo "{\"percentage\":\""''${connection[0]}"\", \"text\":\""$text"\", \"tooltip\":\""$tooltip"\", \"class\":\"1\"}"
       # Use the control socket to close the ssh tunnel.
-      ${pkgs.openssh}/bin/ssh -q -S /tmp/nmcli_socket -O exit ghaf@${netvm_address}
-    '';
-in
-  stdenvNoCC.mkDerivation {
-    name = "wifi-signal-strength";
-
-    phases = ["installPhase"];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp ${wifiSignalStrength} $out/bin/wifi-signal-strength
+      ${openssh}/bin/ssh -q -S /tmp/nmcli_socket -O exit ghaf@${netvm_address}
     '';
 
     meta = with lib; {
